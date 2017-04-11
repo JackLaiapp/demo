0823f9da0b3c8d3cf611e2ef00e23a21a9535a15
MyMac <mymac@mymac-no-macbook-pro.local>
2017-04-11 17:12:08 +0800

    add delete

--- babyphoto/babyphoto/ViewController.swift
+++ babyphoto/babyphoto/ViewController.swift
@@ -31,28 +31,45 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    // 有幾個 section
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
-    
+    // 必須實作的方法：每一組有幾個 cell
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return info.count
     }
-    
+    // 必須實作的方法：每個 cell 要顯示的內容
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! InfoCollectionViewCell
         let data = info[indexPath.row]
         if data.image != nil{
         cell.babyImageView.image = UIImage(data: data.image as! Data)
+
         }
         
         cell.babyTitleLabel.text = data.title
-        
 
         return cell
     }
+    // 點選 cell 後執行的動作
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let data = info[indexPath.row]
+        context.delete(data)
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        do{
+            info = try context.fetch(Info.fetchRequest())
+        }
+        catch{
+            print("Fetching Failed")
+        }
+        self.collectionView.reloadData()
 
+    }
+    
     func getdata(){
         
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
@@ -65,7 +82,6 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         }
         
     }
-    
 
 }
 
