33f737b58957182690bbcc719c90aef0f0014208
MyMac <mymac@mymac-no-macbook-pro.local>
2017-04-14 17:39:40 +0800

    update select photo function

--- babyphoto/babyphoto/AddViewController.swift
+++ babyphoto/babyphoto/AddViewController.swift
@@ -14,8 +14,8 @@ class AddViewController: UIViewController, UIImagePickerControllerDelegate, UINa
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet var location: UITextField!
     
-    @IBAction func selectPhoto(_ sender: Any) {
-        //建立一個ImagePickerController
+    @IBAction func selectImage(_ sender: Any) {
+/*        //建立一個ImagePickerController
         let imagePicker = UIImagePickerController()
         //實作UIImagePickerControllerDelegate的物件
         imagePicker.delegate = self
@@ -24,9 +24,34 @@ class AddViewController: UIViewController, UIImagePickerControllerDelegate, UINa
         //指定從相簿中挑選相片
         imagePicker.sourceType = .photoLibrary
         //顯示UIImagePickerController
+
         self.present(imagePicker, animated: true, completion: nil)
+*/
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
         
+        let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a source", preferredStyle: .actionSheet)
         
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action:UIAlertAction) in
+            
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true, completion: nil)
+            }else{
+                print("Camera not available")
+            }
+            
+            
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { (action:UIAlertAction) in
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(actionSheet, animated: true, completion: nil)
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
@@ -38,18 +63,36 @@ class AddViewController: UIViewController, UIImagePickerControllerDelegate, UINa
     
     
     @IBAction func savebtn(_ sender: Any) {
-        
+        if imageView.image != nil{
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         
         let info = Info(context: context)
-        
+
         info.image = UIImagePNGRepresentation(imageView.image!)! as NSData?
         info.title = textField.text!
         info.location = location.text!
         (UIApplication.shared.delegate as! AppDelegate).saveContext()
         navigationController!.popViewController(animated: true)
+        }else{
+         noselectImage()   
+        }
     }
     
+    func noselectImage(){
+        let alertVC = UIAlertController(
+            title: "No Select Photo",
+            message: "Sorry, Please Select Photo",
+            preferredStyle: .alert)
+        let okAction = UIAlertAction(
+            title: "OK",
+            style:.default,
+            handler: nil)
+        alertVC.addAction(okAction)
+        present(
+            alertVC,
+            animated: true,
+            completion: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
